e5831d8c06df9267d1cfd098deefac4a4db78de7
shaoxiaojing123 <381068123@qq.com>
2019-09-07 11:51:28 +0800

    shaoxj冲突合并

--- src/apis/index.js
+++ src/apis/index.js
@@ -13,11 +13,6 @@ export const authMgmt = params => {
   return Axios.get(`${BASE_PATH}/getManagerList`).then(res => res.data)
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// 查看当前房间状态分布
-=======
-=======
 // 超级管理员新增管理员
 export const register = params => {
   return Axios.post(`${BASE_PATH}/insertManager`).then(res => res.data)
@@ -33,13 +28,11 @@ export const deleteManager = params => {
   return Axios.post(`${BASE_PATH}/deleteManager`).then(res => res.data)
 }
 
->>>>>>> ecf3a3221417dbd86ce06dd5d9fc17ab84ba0428
 // 查询所有房间信息
 export const roomStateList = params => {
   return Axios.get(`${BASE_PATH}/getRoomStateList`).then(res => res.data)
 }
 // 查询当前房间状态分布
->>>>>>> e3348a64b990f9347203f0409edaca0bd5decb30
 export const roomStatus = params => {
   return Axios.get(`${BASE_PATH}/hotelMgmt/incomeStatistics/getRoomList`).then(res => res.data)
 }
--- src/mocks/config.js
+++ src/mocks/config.js
@@ -3,20 +3,12 @@ const PORT = '8082'
 const API_URL = {
   // homeloadData: '/home/loadData',
   managerlist: '/getManagerList',
-<<<<<<< HEAD
-<<<<<<< HEAD
-  roomStatus: '/hotelMgmt/incomeStatistics/getRoomList',
-  incomeStatus: '/hotelMgmt/incomeStatistics/getIncome'
-=======
-=======
   registerManager: '/insertManager',
   updateManager: '/updateManager',
   deleteManager: '/deleteManager',
   resetManagerPwd: '/updateManager',
->>>>>>> ecf3a3221417dbd86ce06dd5d9fc17ab84ba0428
   roomStateList: '/getRoomStateList',
   roomStatus: '/hotelMgmt/incomeStatistics/getRoomList'
->>>>>>> e3348a64b990f9347203f0409edaca0bd5decb30
 }
 
 export default {
